83698623d5a81d1a2e5474dd1c4e00684ef21af5
Roga <ryanroga@gmail.com>
2022-10-17 12:25:28 -0700

    Update TopBar.vue

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,8 +1,12 @@
+<script setup lang="ts">
+import Weather from "./Weather.vue";
+</script>
+
 <template>
   <div class="w-full font-semibold text-white absolute top-0 py-6 px-10 z-10">
     <div class="flex flex-row justify-between items-center">
       <h1>Roga Pokemon</h1>
-      <p>{Weather}</p>
+      <Weather />
     </div>
   </div>
 </template>
